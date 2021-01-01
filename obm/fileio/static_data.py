0b00e1de77305c113e39e8958d5a6d83e042d796
Harald Wilhelmi <harald@hwwey.de>
2021-01-01 16:15:46 +0100

    Cleanup: Consolidated static data in data directory

--- obm/fileio/static_data.py
+++ obm/fileio/static_data.py
@@ -1,14 +1,9 @@
 from os.path import dirname, join
 
-_IMAGE_DIR = join(dirname(dirname(dirname(__file__))), 'images')
+_IMAGE_DIR = join(dirname(dirname(dirname(__file__))), 'data')
 _DEFAULT_MAP = join(_IMAGE_DIR, 'default_map.svg')
 
 
 def get_default_map() -> bytes:
     with open(join(_IMAGE_DIR, 'default_map.svg'), 'rb') as fp:
-        return fp.read()
-
-
-def get_default_token() -> bytes:
-    with open(join(_IMAGE_DIR, 'default_token.svg'), 'rb') as fp:
         return fp.read()
