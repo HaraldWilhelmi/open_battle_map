afdc5e9a2e56c6856e21790ce20f9a11d9d19648
Harald Wilhelmi <harald@hwwey.de>
2021-01-02 01:16:23 +0100

    Refactoring directory structure

--- deploy/build.sh
+++ deploy/build.sh
@@ -8,12 +8,12 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 cd ../obm_react
 npm run build
 
-cd ../deploy
+cd ../deploy/docker
 
 rm -rf build/srv/www || true
 rm -rf build/srv/app || true
 mkdir -p build/srv/{www,app}
-cp -r ../obm_react/build/* build/srv/www
-cp -r ../obm_server/{obm,data,requirements.txt} build/srv/app
+cp -r ../../obm_react/build/* build/srv/www
+cp -r ../../obm_server/{obm,data,requirements.txt} build/srv/app
 
 docker build -t open_battle_map .
